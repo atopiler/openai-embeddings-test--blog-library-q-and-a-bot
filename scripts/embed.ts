8a4f10b714c1f1234f5f0cd59a78eeadc6ac2ba4
alex <alex@tailwindapp.com>
2023-03-23 17:55:15 -0400

    remove one-time stuff

--- scripts/embed.ts
+++ scripts/embed.ts
@@ -18,45 +18,45 @@ const generateEmbeddings = async (essays: PGEssay[]) => {
     for (let i = 0; i < essays.length; i++) {
         const essay = essays[i];
 
-        if (i > 88) {
-            for (let j = 0; j < essay.chunks.length; j++) {
-                const chunk = essay.chunks[j];
+        
+        for (let j = 0; j < essay.chunks.length; j++) {
+            const chunk = essay.chunks[j];
 
-                if ((i == 89 && j > 6) || i >89) {
-                    //create embedding from chunk
-                    const embeddingResponse = await openai.createEmbedding({
-                        model: 'text-embedding-ada-002',
-                        input: chunk.content
-                    });
+        
+            //create embedding from chunk
+            const embeddingResponse = await openai.createEmbedding({
+                model: 'text-embedding-ada-002',
+                input: chunk.content
+            });
 
-                    //destructure data into embedding object
-                    const [{embedding}] = embeddingResponse.data.data;
+            //destructure data into embedding object
+            const [{embedding}] = embeddingResponse.data.data;
 
-                    //upload embedding to DB
-                    const {data, error} = await supabase
-                        .from('paul_graham_essays')
-                        .insert({
-                            essay_title: chunk.essay_title,
-                            essay_url: chunk.essay_url,
-                            essay_date: chunk.essay_url,
-                            content: chunk.content,
-                            content_tokens: chunk.content_tokens,
-                            embedding
-                        })
-                        .select("*");
+            //upload embedding to DB
+            const {data, error} = await supabase
+                .from('paul_graham_essays')
+                .insert({
+                    essay_title: chunk.essay_title,
+                    essay_url: chunk.essay_url,
+                    essay_date: chunk.essay_date,
+                    content: chunk.content,
+                    content_tokens: chunk.content_tokens,
+                    embedding
+                })
+                .select("*");
 
-                        if (error) {
-                            console.log('error', i, j);
-                        } else {
-                            console.log('saved', i, j);
-                            console.log(essay.title);
-                        }
-
-                        //add delay to avoid any potential rate limit issues
-                        await new Promise(resolve => setTimeout(resolve, 300));
+                if (error) {
+                    console.log('error', i, j);
+                } else {
+                    console.log('saved', i, j);
+                    console.log(essay.title);
                 }
-            }
+
+                //add delay to avoid any potential rate limit issues
+                await new Promise(resolve => setTimeout(resolve, 300));
+            
         }
+        
     }
 }
 
